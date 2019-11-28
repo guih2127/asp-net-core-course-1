a13ab2f02f23d305f1f42bcaae887f957199285c
Guilherme Reis Araújo <guilherme.reis@sys10.me>
2019-11-28 18:54:22 -0300

    Conclusão da Aula 04 - Exibindo o Catalogo de Produtos

---
+++ project/Aulas/Aula1/CasaDoCodigo/Repositories/IProdutoRepository.cs
@@ -0,0 +1,15 @@
+﻿using CasaDoCodigo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static CasaDoCodigo.Repositories.ProdutoRepository;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface IProdutoRepository
+    {
+        void SaveProdutos(List<Livro> livros);
+        IList<Produto> GetProdutos();
+    }
+}
